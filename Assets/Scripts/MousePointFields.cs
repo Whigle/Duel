438599eb8ff747a77a3cdab75c963aa9a7ae0c89
Whigle-Komputer\Whigle <i.bro.n.man@gmail.com>
2016-11-10 22:17:08 +0100

    Dodanie oznaczania pól, podpowiada ale ostatnie zaznaczanie nie zamyka podpowiedzi ... to smutne ;(

--- Assets/Scripts/MousePointFields.cs
+++ Assets/Scripts/MousePointFields.cs
@@ -94,6 +94,8 @@ public class MousePointFields : MonoBehaviour {
 				check=false;	
 				pierwszezaznaczone = null;
 				drugiezaznaczone = null;
+				visibility=false;	//zrób niewidocznym
+				target=Color.white;
 			}
 			//jeśli nie zostaly zaznaczone żadne pola, to przywróć tło do normalnego koloru
 			else if (countMarkedFields()==0){
@@ -122,7 +124,7 @@ public class MousePointFields : MonoBehaviour {
 				gameObject.GetComponent<Renderer>().enabled=visibility;
 			}
 			//przypisanie koloru pola do renderera obiektu pola
-			gameObject.GetComponent<Renderer>().material.color=target;
+			czyPokolorowac();
 		}
 		//jeśli HUD jest nieaktywny
 		else{
@@ -140,49 +142,9 @@ public class MousePointFields : MonoBehaviour {
 		//jeśli można zaznaczać
 		if(check){
 			//jeśli jest wciśnięty LPM
-			if (Input.GetMouseButton(0)){
-				target=Color.white;	//ustaw kolor pola na biały
-				if (countMarkedFields()<3){ //hitbox[idx,idy] = true;	//przestaw flagę pola na zaznaczone (prawda)
-					if (pierwszezaznaczone==null){
-						pierwszezaznaczone=this.gameObject;
-						hitbox[idx,idy]=true;
-					} 
-					else if ((pierwszezaznaczone!=this.gameObject)&&(drugiezaznaczone==null)){
-						if ((Mathf.Abs(pierwszezaznaczone.GetComponent<MousePointFields>().idx-idx)<=1)&&(Mathf.Abs(pierwszezaznaczone.GetComponent<MousePointFields>().idy-idy)<=1)){
-							drugiezaznaczone=this.gameObject;
-							hitbox[idx,idy]=true;
-						}
-					} 
-					else if ((pierwszezaznaczone!=null)&&(pierwszezaznaczone!=this.gameObject)&&(drugiezaznaczone!=null)&&(drugiezaznaczone!=this.gameObject)){
-						int p1x=pierwszezaznaczone.GetComponent<MousePointFields>().idx;
-						int p1y=pierwszezaznaczone.GetComponent<MousePointFields>().idy;
-						int p2x=drugiezaznaczone.GetComponent<MousePointFields>().idx;
-						int p2y=drugiezaznaczone.GetComponent<MousePointFields>().idy;
-						if ((p1x!=p2x)&&(p1y!=p2y)){
-							if ((Mathf.Abs(p2x-idx)<=1)&&(Mathf.Abs(p2y-idy)<=1)&&(p1x!=idx)&&(p1y!=idy)){
-								ostatniezaznaczone=this.gameObject;
-								hitbox[idx,idy]=true;
-							} 
-						} 
-						else if (((p1x==p2x)&&(p1y!=p2y))||((p1x!=p2x)&&(p1y==p2y))){
-							if ((Mathf.Abs(p2x-idx)<=1)&&(Mathf.Abs(p2y-idy)<=1)&&((Mathf.Abs(p1x-idx)>1)||(Mathf.Abs(p1y-idy)>1))){
-								hitbox[idx,idy]=true;
-							}
-						} 
-						if (((p1x!=1)||(p1y!=1))&&((p2x!=1)||(p2y!=1))){
-							if(((idx!=1)||(idy!=1))&&(Mathf.Abs(p2x-idx)<=1)&&(Mathf.Abs(p2y-idy)<=1)){
-								hitbox[idx,idy]=true;
-							}
-						}
-					}
-				}
-			
-			}
+			if (Input.GetMouseButton(0)) czyMoznaZaznaczyc ();
 			//jeśli jest wciśnięty PPM
-			else if (Input.GetMouseButton(1)){
-				target = Color.white;	//ustaw kolor pola na biały
-				if (countMarkedFields()<3) hitbox[idx,idy] = true;	//przestaw flagę pola na zaznaczone (prawda)
-			}
+			else if (Input.GetMouseButton(1)) czyMoznaZaznaczyc ();
 			//jeśli nie jest wciśnięty żaden przycisk
 			else {
 				visibility=true; //zrób pole widocznym
@@ -234,6 +196,21 @@ public class MousePointFields : MonoBehaviour {
 		check=true;
 		clearHitbox();
 		mode=false;
+
+		pierwszezaznaczone = null;
+		drugiezaznaczone = null;
+		ostatniezaznaczone = null;
+
+		GameObject.Find ("TopLeftCombatField").GetComponent<MousePointFields> ().visibility = false;
+		GameObject.Find ("TopCombatField").GetComponent<MousePointFields> ().visibility = false;
+		GameObject.Find ("TopRightCombatField").GetComponent<MousePointFields> ().visibility = false;
+		GameObject.Find ("LeftCombatField").GetComponent<MousePointFields> ().visibility = false;
+		GameObject.Find ("MiddleCombatField").GetComponent<MousePointFields> ().visibility = false;
+		GameObject.Find ("RightCombatField").GetComponent<MousePointFields> ().visibility = false;
+		GameObject.Find ("BottomLeftCombatField").GetComponent<MousePointFields> ().visibility = false;
+		GameObject.Find ("BottomCombatField").GetComponent<MousePointFields> ().visibility = false;
+		GameObject.Find ("BottomRightCombatField").GetComponent<MousePointFields> ().visibility = false;
+
 	}
 
 	/// <summary>
@@ -304,5 +281,106 @@ public class MousePointFields : MonoBehaviour {
 		str+="Marked: ";
 		str+=countMarkedFields().ToString();
 		print(str);
+	}
+
+	public void czyMoznaZaznaczyc() {
+		target = Color.white;
+		if (countMarkedFields () < 3) {
+			if (pierwszezaznaczone == null) {
+				pierwszezaznaczone = this.gameObject;
+				hitbox [idx, idy] = true;
+			} 
+			if (pierwszezaznaczone != null && pierwszezaznaczone != this.gameObject && drugiezaznaczone == null) {
+				if ((pierwszezaznaczone.GetComponent<MousePointFields> ().idx != 1 && pierwszezaznaczone.GetComponent<MousePointFields> ().idy != 1) || (pierwszezaznaczone.GetComponent<MousePointFields> ().idx == 1 && pierwszezaznaczone.GetComponent<MousePointFields> ().idy == 1)) {
+					if (Mathf.Abs (pierwszezaznaczone.GetComponent<MousePointFields> ().idx - idx) <= 1 && Mathf.Abs (pierwszezaznaczone.GetComponent<MousePointFields> ().idy - idy) <= 1) {
+						drugiezaznaczone = this.gameObject;
+						hitbox [idx, idy] = true;
+					}
+				} 
+				if (!(pierwszezaznaczone.GetComponent<MousePointFields> ().idx != 1 && pierwszezaznaczone.GetComponent<MousePointFields> ().idy != 1) || !(pierwszezaznaczone.GetComponent<MousePointFields> ().idx == 1 && pierwszezaznaczone.GetComponent<MousePointFields> ().idy == 1)) {
+					if ((Mathf.Abs (pierwszezaznaczone.GetComponent<MousePointFields> ().idx - idx) <= 1 && pierwszezaznaczone.GetComponent<MousePointFields> ().idy == idy) || (Mathf.Abs (pierwszezaznaczone.GetComponent<MousePointFields> ().idy - idy) <= 1 && pierwszezaznaczone.GetComponent<MousePointFields> ().idx == idx)) {
+						drugiezaznaczone = this.gameObject;
+						hitbox [idx, idy] = true;
+					}
+				}
+			} else if (pierwszezaznaczone != null && pierwszezaznaczone != this.gameObject && drugiezaznaczone != null && drugiezaznaczone != this.gameObject) {
+				int p1x = pierwszezaznaczone.GetComponent<MousePointFields> ().idx;
+				int p1y = pierwszezaznaczone.GetComponent<MousePointFields> ().idy;
+				int p2x = drugiezaznaczone.GetComponent<MousePointFields> ().idx;
+				int p2y = drugiezaznaczone.GetComponent<MousePointFields> ().idy;
+				print (p1x + " " + p1y + " " + p2x + " " + p2y);
+				if (p1x != p2x && p1y != p2y) {
+					if (Mathf.Abs (p2x - idx) <= 1 && Mathf.Abs (p2y - idy) <= 1 && p1x != idx && p1y != idy) {
+						hitbox [idx, idy] = true;
+					} 
+				} 
+				if ((p1x == p2x && p1y != p2y) || (p1x != p2x && p1y == p2y)) {
+					if (p1x == 1 && p1y == 1) {
+					} else if (p2x == 1 && p2y == 1) {
+						if ((Mathf.Abs (p2x - idx) <= 1 && Mathf.Abs (p2y - idy) <= 1) && (Mathf.Abs (p1x - idx) > 1 || Mathf.Abs (p1y - idy) > 1)) {
+							hitbox [idx, idy] = true;
+						}
+					} else if ((p1x == 1 || p1y == 1) && (p2x != 1 && p2y != 1)) {
+						if((Mathf.Abs (p2x - idx) <= 1 && p2y == idy) || (Mathf.Abs (p2y - idy) <= 1 && p2x == idx)) {
+							hitbox [idx, idy] = true;
+						}
+					} else {
+						if (((Mathf.Abs (p2x - idx) <= 1 && p2y == idy) || (Mathf.Abs (p2y - idy) <= 1 && p2x == idx)) && this.gameObject != pierwszezaznaczone && (idx!=1&&idy!=1)) {
+							hitbox [idx, idy] = true;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	public void czyPokolorowac() {
+		if (pierwszezaznaczone != null && pierwszezaznaczone != this.gameObject && drugiezaznaczone == null) {
+			if ((pierwszezaznaczone.GetComponent<MousePointFields> ().idx != 1 && pierwszezaznaczone.GetComponent<MousePointFields> ().idy != 1) || (pierwszezaznaczone.GetComponent<MousePointFields> ().idx == 1 && pierwszezaznaczone.GetComponent<MousePointFields> ().idy == 1)) {
+				if (Mathf.Abs (pierwszezaznaczone.GetComponent<MousePointFields> ().idx - idx) <= 1 && Mathf.Abs (pierwszezaznaczone.GetComponent<MousePointFields> ().idy - idy) <= 1) {
+					visibility = true;
+					gameObject.GetComponent<Renderer> ().material.color = Color.yellow;
+				}
+			} 
+			if (!(pierwszezaznaczone.GetComponent<MousePointFields> ().idx != 1 && pierwszezaznaczone.GetComponent<MousePointFields> ().idy != 1) || !(pierwszezaznaczone.GetComponent<MousePointFields> ().idx == 1 && pierwszezaznaczone.GetComponent<MousePointFields> ().idy == 1)) {
+				if ((Mathf.Abs (pierwszezaznaczone.GetComponent<MousePointFields> ().idx - idx) <= 1 && pierwszezaznaczone.GetComponent<MousePointFields> ().idy == idy) || (Mathf.Abs (pierwszezaznaczone.GetComponent<MousePointFields> ().idy - idy) <= 1 && pierwszezaznaczone.GetComponent<MousePointFields> ().idx == idx)) {
+					visibility = true;
+					gameObject.GetComponent<Renderer> ().material.color = Color.yellow;
+				}
+			}
+		} else if (pierwszezaznaczone != null && pierwszezaznaczone != this.gameObject && drugiezaznaczone != null && drugiezaznaczone != this.gameObject) {
+			visibility = false;	//zrób niewidocznym
+			target = Color.white;
+			int p1x = pierwszezaznaczone.GetComponent<MousePointFields> ().idx;
+			int p1y = pierwszezaznaczone.GetComponent<MousePointFields> ().idy;
+			int p2x = drugiezaznaczone.GetComponent<MousePointFields> ().idx;
+			int p2y = drugiezaznaczone.GetComponent<MousePointFields> ().idy;
+			print (p1x + " " + p1y + " " + p2x + " " + p2y);
+			if (p1x != p2x && p1y != p2y) {
+				if (Mathf.Abs (p2x - idx) <= 1 && Mathf.Abs (p2y - idy) <= 1 && p1x != idx && p1y != idy) {
+					visibility = true;
+					gameObject.GetComponent<Renderer> ().material.color = Color.yellow;
+				} 
+			} 
+			if ((p1x == p2x && p1y != p2y) || (p1x != p2x && p1y == p2y)) {
+				if (p1x == 1 && p1y == 1) {
+				} else if (p2x == 1 && p2y == 1) {
+					if ((Mathf.Abs (p2x - idx) <= 1 && Mathf.Abs (p2y - idy) <= 1) && (Mathf.Abs (p1x - idx) > 1 || Mathf.Abs (p1y - idy) > 1)) {
+						visibility = true;
+						gameObject.GetComponent<Renderer> ().material.color = Color.yellow;
+					}
+				} else if ((p1x == 1 || p1y == 1) && (p2x != 1 && p2y != 1)) {
+					if ((Mathf.Abs (p2x - idx) <= 1 && p2y == idy) || (Mathf.Abs (p2y - idy) <= 1 && p2x == idx)) {
+						visibility = true;
+						gameObject.GetComponent<Renderer> ().material.color = Color.yellow;
+					}
+				} else {
+					if (((Mathf.Abs (p2x - idx) <= 1 && p2y == idy) || (Mathf.Abs (p2y - idy) <= 1 && p2x == idx)) && this.gameObject != pierwszezaznaczone && (idx != 1 && idy != 1)) {
+						visibility = true;
+						gameObject.GetComponent<Renderer> ().material.color = Color.yellow;
+					}
+				}
+			}
+		} else gameObject.GetComponent<Renderer>().material.color=target;
 	}
 }
